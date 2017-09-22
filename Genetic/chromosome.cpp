25fe5aa3700fe2cc00e031c24d0bfbb20b94b132
Kerndog73 <kerndog73@gmail.com>
2017-09-22 20:27:01 +0930

    Add chromosome

---
+++ Genetic/chromosome.cpp
@@ -0,0 +1,9 @@
+//
+//  chromosome.cpp
+//  Genetic
+//
+//  Created by Indi Kernick on 22/9/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include "chromosome.hpp"
---
+++ Genetic/chromosome.hpp
@@ -0,0 +1,17 @@
+//
+//  chromosome.hpp
+//  Genetic
+//
+//  Created by Indi Kernick on 22/9/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef chromosome_hpp
+#define chromosome_hpp
+
+#include <vector>
+
+template <typename Allele>
+using Chromosome = std::vector<Allele>;
+
+#endif
