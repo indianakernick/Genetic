8903a4f6b2b1ef51d105cb20eebf68af4983b907
Kerndog73 <kerndog73@gmail.com>
2017-09-22 21:00:58 +0930

    Rewrite Population

--- Genetic/population.hpp
+++ Genetic/population.hpp
@@ -14,21 +14,22 @@
 #include "chromosome.hpp"
 
 template <typename Chromosome>
-class Population {
-public:
-  Population(const size_t size, const Allele<Chromosome> seed)
-    : population(size) {
-    std::mt19937_64 generator(seed);
-    std::uniform_int_distribution<Allele<Chromosome>> dist;
-    for (Chromosome &c : population) {
-      for (Allele<Chromosome> &a : c) {
-        a = dist(generator);
-      }
+using Population = std::vector<Chromosome>;
+
+template <typename Chromosome>
+Population<Chromosome> makePopulation(const size_t size) {
+  return {size};
+}
+
+template <typename Chromosome>
+void initRandom(Population<Chromosome> &pop, const Allele<Chromosome> seed) {
+  std::mt19937_64 gen(seed);
+  std::uniform_int_distribution<Allele<Chromosome>> dist;
+  for (Chromosome &c : pop) {
+    for (Allele<Chromosome> &a : c) {
+      a = dist(gen);
     }
   }
-  
-private:
-  std::vector<Chromosome> population;
-};
+}
 
 #endif
